6d56ffc80ae2d6ff7b73606fbfeb46289430c749
lenabenoy <lenabenoy2029@cs.sjcetpalai.ac.in>
2026-02-15 08:04:03 +0530

    Create copy string handling.c
    adding  copy string using string handling functions

---
+++ string/copy string handling.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    // Write C code here
+    char str1[100],str2[100
+    printf("enter a string\n");
+    fgets(str1,sizeof(str1),stdin);
+    strcpy(str2,str1);
+    printf("copied string is%s",str2);
+
+    return 0;
+}
